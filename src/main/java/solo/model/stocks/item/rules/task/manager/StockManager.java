4579ed22327151eb4a480fe40a5b175b00869f5c
solo-vey <y.y.solovey@gmail.com>
2018-02-13 11:15:41 +0200

    Остановка одного из нескольких контролеров по одной паре при продаже в минус

--- src/main/java/solo/model/stocks/item/rules/task/manager/StockManager.java
+++ src/main/java/solo/model/stocks/item/rules/task/manager/StockManager.java
@@ -6,27 +6,99 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.apache.commons.lang.StringUtils;
+
 import solo.CurrencyInformer;
 import solo.model.stocks.analyse.StateAnalysisResult;
 import solo.model.stocks.exchange.IStockExchange;
+import solo.model.stocks.item.IRule;
+import solo.model.stocks.item.RateInfo;
+import solo.model.stocks.item.rules.task.trade.ITradeControler;
 import solo.model.stocks.item.rules.task.trade.TaskTrade;
+import solo.model.stocks.item.rules.task.trade.TradeControler;
+import solo.model.stocks.item.rules.task.trade.TradeUtils;
 import solo.model.stocks.worker.WorkerFactory;
 import ua.lz.ep.utils.ResourceUtils;
 
 public class StockManager implements IStockManager
 {
 	final protected StockManagesInfo m_oStockManagesInfo;
+	final protected boolean m_bIsTrackTrades;
 	
 	public StockManager(final IStockExchange oStockExchange)
 	{
 		m_oStockManagesInfo = load(oStockExchange);
+		m_bIsTrackTrades = true;
 	}
 	
 	public void manage(final StateAnalysisResult oStateAnalysisResult) 
 	{
+	}	
+	
+	void trackTrades(final TaskTrade oTaskTrade)
+	{
+		if (!m_bIsTrackTrades)
+			return;
 		
+		final BigDecimal nTradeDelta = oTaskTrade.getTradeInfo().getDelta();
+		if (nTradeDelta.compareTo(BigDecimal.ZERO) < 0)
+			stopControler(oTaskTrade.getTradeInfo().getRateInfo(), oTaskTrade);
+		else
+			startAllControlers(oTaskTrade.getTradeInfo().getRateInfo(), oTaskTrade);
 	}
 	
+	private void stopControler(final RateInfo oRateInfo, final TaskTrade oTaskTrade)
+	{
+		final List<Entry<Integer, IRule>> oRules = WorkerFactory.getStockExchange().getRules().getRules(oRateInfo);
+		final List<ITradeControler> aWorkingControler = new LinkedList<ITradeControler>();
+		for(final Entry<Integer, IRule> oRuleInfo : oRules)
+		{
+			final IRule oRule = oRuleInfo.getValue();
+			final ITradeControler oControler = TradeUtils.getRuleAsTradeControler(oRule);
+			if (null == oControler)
+				continue;
+			
+			if (oControler.getParameter(TradeControler.TRADE_COUNT_PARAMETER).equalsIgnoreCase("0"))
+				continue;
+			
+			aWorkingControler.add(oControler);
+		}
+		
+		if (aWorkingControler.size() > 1)
+		{
+			oTaskTrade.getTradeControler().setParameter(TradeControler.TRADE_COUNT_PARAMETER, "0");
+			WorkerFactory.getTransport().sendMessage("MANAGER\r\nStop controler [" + oTaskTrade.getID() + "]");
+		}
+	}
+		
+	private void startAllControlers(final RateInfo oRateInfo, final TaskTrade oTaskTrade)
+	{
+		final List<Entry<Integer, IRule>> oRules = WorkerFactory.getStockExchange().getRules().getRules(oRateInfo);
+		String strMessage = StringUtils.EMPTY  + "";
+		for(final Entry<Integer, IRule> oRuleInfo : oRules)
+		{
+			final IRule oRule = oRuleInfo.getValue();
+			final ITradeControler oControler = TradeUtils.getRuleAsTradeControler(oRule);
+			if (null == oControler)
+				continue;
+			
+			final String strMaxTrades = oControler.getParameter(TradeControler.TRADE_COUNT_PARAMETER);
+			if (!strMaxTrades.equalsIgnoreCase("0"))
+				continue;
+			
+			oControler.getTradesInfo().getHistory().addToLog("Manager.startAllControlers.");
+			strMessage += "Start controler [" + oRule.getID() + "]\r\n";	
+			oControler.setParameter(TradeControler.TRADE_COUNT_PARAMETER, oControler.getParameter(TradeControler.MAX_TARDES));
+		}
+		
+		if (StringUtils.isNotBlank(strMessage))
+			WorkerFactory.getTransport().sendMessage("MANAGER\r\n" + strMessage);
+	}
+
 	@Override public StockManagesInfo getInfo()
 	{
 		return m_oStockManagesInfo;
@@ -42,8 +114,10 @@ public class StockManager implements IStockManager
 	{
 		m_oStockManagesInfo.tradeDone(oTaskTrade);
 		save();
+		
+		trackTrades(oTaskTrade);
 	}
-	
+
 	@Override public void buyDone(final TaskTrade oTaskTrade) 
 	{
 		m_oStockManagesInfo.buyDone(oTaskTrade);
