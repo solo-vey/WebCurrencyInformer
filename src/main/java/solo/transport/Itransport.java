12609e1abe8a499bcb43f03fe8fbbbc6157aa22c
solo-vey <y.y.solovey@gmail.com>
2018-02-13 11:16:32 +0200

    Рефакторинг

--- src/main/java/solo/transport/Itransport.java
+++ src/main/java/solo/transport/Itransport.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public interface ITransport
 {
-	Object sendMessage(final String strText) throws Exception;
+	Object sendMessage(final String strText);
 	void sendPhoto(final File oPhoto, String strCaption) throws Exception;
 	void deleteMessage(String strMessageID) throws Exception;
 	ITransportMessages getMessages() throws Exception;
